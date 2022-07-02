f89daacd1106958da1aa6c6d902200cecb2cd185
RADIOFAN <radiofan22@mail.ru>
2022-07-02 15:13:35 +0700

    + обработка аргуметнов командной строки + CppDoc

--- happy_birthday/main.cpp
+++ happy_birthday/main.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
 #include <windowsx.h>
+#pragma comment(lib, "Shlwapi.lib")
+#include <shlwapi.h>
 //#include <strsafe.h>
 //#include <commctrl.h>
 #include <string>
@@ -8,6 +10,9 @@
 #define WM_SHELLNOTIFY (WM_APP + 1)
 #define CM_EXIT 15
 #define CM_EXEC 14
+#define DEFAULT_CLOSE_AFTER_SEC 5
+#define STR_(X) #X
+#define STR(X) STR_(X)
 
 LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam);
 
@@ -22,19 +27,84 @@ private:
 	HINSTANCE hInstance;
 	HMENU context_menu;
 
+
+	//параметры задаются параметрами запуска
+	bool args_parse_result;
+
+	/**
+	* вывести справку
+	* --help -h /? -?
+	*/
+	bool is_help;
+	/**
+	* закрыть программу после отображения всех уведомлений через указанное количество сек
+	* если < 0 программа не закроется
+	* /c[ количество ожидания в сек]
+	* -c[ количество ожидания в сек]
+	* --close[ количество ожидания в сек]
+	*/
+	int close_after_sec;
+
 public:
-	App(HINSTANCE hInstance) : hInstance(hInstance), main_win_c({0}), window(nullptr), icon_struct({0}), context_menu(nullptr){
+	/**
+	* главный класс программы, создать и вернуть в главной функции App::start
+	* @param hInstance - 
+	* @param comand_args - указатель на строку с аргументами
+	*/
+	App(HINSTANCE hInstance, LPTSTR comand_args)
+		: icon_struct({0}),
+		main_win_c({0}),
+		window(nullptr),
+		hInstance(hInstance),
+		context_menu(nullptr),
+
+		is_help(false),
+		close_after_sec(-1)
+	{
+		args_parse_result = this->parse_comand_args(comand_args);
+		if(!args_parse_result || is_help)
+			return;
 		window = create_window();
 		this->init_icon_struct();
 		this->init_context_menu();
 	}
 
+	/**
+	* Удаляет значок из трея
+	* @see App::icon_struct
+	*/
 	void hide_icon(){
-		// Удаление значка
 		Shell_NotifyIcon(NIM_DELETE, &icon_struct);
 	}
 
+	/**
+	* запускает обработку событий
+	* или выводит уведомление об ошибке парсинга параметров (args_parse_result == false)
+	* @see App::args_parse_result
+	* или выводит справку (is_help == true)
+	* @see App::is_help
+	* @return код завершения работы приложения
+	*/
 	int start(){
+		if(!args_parse_result){
+			MessageBox(NULL, TEXT("Ошибка распознвания параметров"), TEXT("Ошибка"), MB_OK | MB_ICONERROR | MB_TASKMODAL);
+			return 1;
+		}
+		if(is_help){
+			MessageBox(
+				NULL,
+				TEXT("Description\n\n")//todo
+				TEXT("app.exe   [/c[ секунды]]   [/?]\n\n")
+				TEXT("  /c (-c, --close)\tПроизведет закрытие программы через указанное количество секунд(целое) (по умолчанию ")
+				TEXT(STR(DEFAULT_CLOSE_AFTER_SEC))
+				TEXT(" сек) после отображения всех уведомлений, если не получает событий\n")
+				TEXT("  /? (-?, -h, --help)\tВывод справки\n"),
+				TEXT("Справка"),
+				MB_OK | MB_ICONINFORMATION | MB_TASKMODAL
+			);
+			return 0;
+		}
+
 		Shell_NotifyIcon(NIM_ADD, &icon_struct);
 		Shell_NotifyIcon(NIM_SETVERSION, &icon_struct);
 
@@ -47,8 +117,10 @@ public:
 		return 0;
 	}
 
+	/**
+	* отображает контекстное меню context_menu в указанных координатах
+	*/
 	bool show_context_menu(INT16 x, INT16 y){
- 
 		SetForegroundWindow(window);
 		bool ret = TrackPopupMenu(
 			context_menu,
@@ -63,6 +135,12 @@ public:
 		return ret;
 	}
 
+	/**
+	* Обрабатывает выбора элемента контекстного меню
+	* @param menu_item_ind - идентификатор элемента меню
+	* CM_EXIT - выход из приложения
+	* 
+	*/
 	void context_menu_click(INT16 menu_item_ind){
 		switch(menu_item_ind){
 			case CM_EXIT:
@@ -74,7 +152,12 @@ public:
 		}
 	}
 
+	/**
+	* Очищает память
+	*/
 	~App(){
+		if(!args_parse_result || is_help)
+			return;
 		//очистка памяти
 		DestroyIcon(icon_struct.hIcon);
 		DestroyIcon(icon_struct.hBalloonIcon);
@@ -82,6 +165,12 @@ public:
 	}
 
 private:
+	/**
+	* регистрирует класс окна main_win_c
+	* @see App::main_win_c
+	* создает окно
+	* @return дескриптор созданного окна
+	*/
 	HWND create_window(){
 		//Регистрация класса окна
 		main_win_c.cbSize = sizeof(WNDCLASSEX);
@@ -94,6 +183,62 @@ private:
 		return CreateWindowEx(0, TEXT("Main"), NULL, 0, 0, 0, 0, 0, NULL, NULL, hInstance, NULL);
 	}
 
+	/**
+	* производит парсинг аргументов командной строки
+	* @param comand_args - указатель на строку с аргументами
+	* @see App::is_help
+	* @see App::close_after_sec
+	* @return удалось ли распарсить аргументы
+	*/
+	bool parse_comand_args(LPTSTR comand_args){
+		#ifdef UNICODE
+			LPWSTR *arg_list;
+			int arg_c;
+
+			arg_list = CommandLineToArgvW(comand_args, &arg_c);
+			if(arg_list == NULL){
+			  return false;
+			}
+			for(int i=0; i<arg_c; i++){
+				if(
+					lstrcmp(arg_list[i], TEXT("-h")) == 0 ||
+					lstrcmp(arg_list[i], TEXT("-?")) == 0 ||
+					lstrcmp(arg_list[i], TEXT("/?")) == 0 ||
+					lstrcmp(arg_list[i], TEXT("--help")) == 0
+				){
+					is_help = true;
+					continue;
+				}
+
+				if(
+					lstrcmp(arg_list[i], TEXT("-c")) == 0 ||
+					lstrcmp(arg_list[i], TEXT("/c")) == 0 ||
+					lstrcmp(arg_list[i], TEXT("--close")) == 0
+				){
+					if(i+1 == arg_c){
+						close_after_sec = DEFAULT_CLOSE_AFTER_SEC;
+						continue;
+					}
+					if(StrToIntEx(arg_list[i+1], STIF_DEFAULT, &close_after_sec)){
+						i++;
+					}else{
+						close_after_sec = DEFAULT_CLOSE_AFTER_SEC;
+					}
+				}
+			}
+
+			LocalFree(arg_list);
+			return true;
+		#else
+			#error "parse_comand_args ANSI undefined"
+			return false;
+		#endif //UNICODE
+	}
+
+	/**
+	* производит инициализацию структуры icon_struct
+	* @see App::icon_struct
+	*/
 	void init_icon_struct(){
 		// Создание значка
 		icon_struct.cbSize = sizeof(NOTIFYICONDATA);
@@ -137,7 +282,11 @@ private:
 		icon_struct.dwInfoFlags = NIIF_USER | NIIF_LARGE_ICON;
 		icon_struct.hBalloonIcon = (HICON) LoadImage(hInstance, MAKEINTRESOURCE(103), IMAGE_ICON, SM_CXSMICON, SM_CYSMICON, LR_DEFAULTCOLOR);
 	}
-
+	
+	/**
+	* производит инициализацию контекстного меню context_menu
+	* @see App::context_menu
+	*/
 	void init_context_menu(){
 		context_menu = CreatePopupMenu();
 		AppendMenu(context_menu, MFT_STRING | MF_CHECKED, CM_EXEC, TEXT("Действие"));
@@ -148,7 +297,15 @@ private:
 App* application = nullptr;
 
 
-// Обработка сообщений
+/**
+* обрабатывает события приложения
+* @see App::create_window
+* @param window - 
+* @param message - 
+* WM_SHELLNOTIFY - Сообщение от значка (App::show_context_menu)
+* WM_COMMAND - событие выбора пункта меню (App::context_menu_click)
+* WM_DESTROY - скрывает значок(App::hide_icon), выход из программы
+*/
 LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam){
 	switch(message){
 		//Сообщение от значка
@@ -175,10 +332,13 @@ LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam
 }
 
 
-// Главная функция
+
+/**
+* точка входа
+*/
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nShowCmd){
 
-	App program(hInstance);
+	App program(hInstance, lpCmdLine);
 
 	application = &program;
 
--- happy_birthday/readme.txt
+++ happy_birthday/readme.txt
@@ -4,3 +4,11 @@ https://docs.microsoft.com/ru-RU/windows/win32/api/shellapi/ns-shellapi-notifyic
 
 http://www.vsokovikov.narod.ru/New_MSDN_API/Menu/fn_appendmenu.htm
 http://narovol.narod.ru/_tbkp/New_MSDN_API/Menu/fn_modifymenu.htm
+
+https://marketplace.visualstudio.com/items?itemName=VisualStudioClient.MicrosoftVisualStudio2017InstallerProjects
+https://www.cyberforum.ru/cpp-builder/thread15180.html
+https://www.cyberforum.ru/win-api/thread130524.html
+
+
+https://docs.microsoft.com/ru-ru/windows/win32/shell/launch
+https://docs.microsoft.com/ru-RU/windows/win32/api/shellapi/nf-shellapi-shellexecutea
